ce5b086d7ce86bebca0da36c617267f6b185c7c8
vhafuwicolman <vhafuwicolman@gmail.com>
2023-02-17 14:48:14 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	char i;
+	
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar("\n");
+	return (0);
+}
