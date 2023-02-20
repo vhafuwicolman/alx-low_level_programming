621e28d164c940cccdfbc18aa6a863b1f704e4a9
vhafuwicolman <vhafuwicolman@gmail.com>
2023-02-20 13:48:40 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,21 +10,24 @@ int main(void)
 {
 	int i;
 	int j;
-	int s;
+	int k;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 1 ; j < 10 ; j++)
 		{
-			for (s = 2 ; s < 10 ; s++)
+			for (k = 2 ; k < 10 ; k++)
 			{
-				if (i < j && j < s)
+				if (i < j && j < k)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
-					putchar(s + '0');
-					putchar(',');
-					putchar(' ');
+					putchar(k + '0');
+					if (i + j + k != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
