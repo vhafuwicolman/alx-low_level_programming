b38f0b7131d486c91a578cec67e68516c0d75384
vhafuwicolman <vhafuwicolman@gmail.com>
2023-02-21 09:15:02 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - starting point
+ * _putchar: print the alphabet putchar
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
