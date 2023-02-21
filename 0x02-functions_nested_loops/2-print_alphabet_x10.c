3775ab2486214bdb378792cf82581cfe918b1897
vhafuwicolman <vhafuwicolman@gmail.com>
2023-02-21 10:55:27 -0800

    main h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10: print 10 times the alphabet in lower case
+ *Return: 0 when successful
+ */
+
+void print_alphabet_x10(void);
+{
+	char i;
+	char j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n')
+	}
+}
