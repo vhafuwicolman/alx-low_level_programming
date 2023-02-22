61c5e8677e5c80ffb327819144635a471fd1c941
vhafuwicolman <vhafuwicolman@gmail.com>
2023-02-22 02:09:14 -0800

    Task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints 9 times table
+ *
+ * Return: 0 when successful
+ */
+
+void times_table(void)
+{
+
+}
