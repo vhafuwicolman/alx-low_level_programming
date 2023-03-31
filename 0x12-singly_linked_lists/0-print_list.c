7643d3344db97056568087afb94fa3c2ce828816
vhafuwicolman <vhafuwicolman@gmail.com>
2023-03-30 19:53:39 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,6 @@
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
- *
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
@@ -20,6 +19,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		s++;
 	}
-
 	return (s);
 }
